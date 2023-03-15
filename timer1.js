37004adeecadb35163a51b4df896b28bd4066ca0
tehi_kim <k.tehi21@gmail.com>
2023-03-15 02:30:06 +0000

    adding edge cases

--- timer1.js
+++ timer1.js
@@ -1,11 +1,16 @@
 const args = process.argv.slice(2);
 args.sort((a,b) => a-b);
-// console.log(args);
+console.log(args);
 
 for (const x of args) {
-  setTimeout(() => {
-    process.stdout.write(`\x07 ${x} seconds \n`);
-  }, x * 1000 );
+  if (args.length === 0) {}
+  if (x <= 0) {}
+  if (x !== Number) {}
+  if (x === Number || x > 0) {
+    setTimeout(() => {
+      process.stdout.write(`\x07 ${x} seconds \n`);
+    }, x * 1000 );
+  }
 }
 
 // process.stdout.write('\x07');
